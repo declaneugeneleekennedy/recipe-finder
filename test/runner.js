3c5baed52689e25e3426b86baaebb831d3f2c447
Declan Kennedy <Declan.Kennedy@thewhiteagency.com.au>
2015-04-04 17:59:58 +1100

    added ingredient model and test, as well as test runner

---
+++ test/runner.js
@@ -0,0 +1,15 @@
+// Test runner
+var reporter = require('nodeunit').reporters.default;
+
+// Tests
+
+var tests = [
+    //'/model/fridge',
+    '/model/ingredient',
+    //'/model/recipe',
+    //'/model/unit'
+];
+
+tests.forEach(function(test) {
+    reporter.run([ __dirname + test + '.js' ]);
+});
